5f6f17ef9396d3057415a251829314eae0e09b13
Jacob Juarez <jacobjuarez1996@gmail.com>
2026-02-11 13:19:58 -0600

    gallery push 2 added frames and exterior

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -11,6 +11,14 @@ import vinyl5 from "../assets/gallery-images/vinyl5.jpg"
 import vinyl6 from "../assets/gallery-images/vinyl6.jpg"
 import vinyl7 from "../assets/gallery-images/vinyl7.jpg"
 import vinyl8 from "../assets/gallery-images/vinyl8.jpg"
+import frame1 from "../assets/gallery-images/frame1.jpg"
+import frame2 from "../assets/gallery-images/frame2.jpg"
+import frame3 from "../assets/gallery-images/frame3.jpg"
+import frame4 from "../assets/gallery-images/frame4.jpg"
+import exterior1 from "../assets/gallery-images/exterior1.jpg"
+import exterior2 from "../assets/gallery-images/exterior2.jpg"
+import exterior3 from "../assets/gallery-images/exterior3.jpg"
+
 
 
 
@@ -37,6 +45,16 @@ export default function Work() {
         <GallerySection
           title="Wall Vinyl"
           images={[vinyl1, vinyl2, vinyl3, vinyl4, vinyl5, vinyl6, vinyl7, vinyl8]}
+        />
+
+        <GallerySection
+          title="Wall Frames"
+          images={[frame1, frame2, frame3, frame4]}
+        />
+
+        <GallerySection
+          title="Exterior Work"
+          images={[exterior1, exterior2, exterior3]}
         />
 
       </div>

78a6b512b3f72c96b68e6765abf5eeed5b8283f8
Jacob Juarez <jacobjuarez1996@gmail.com>
2026-02-11 09:23:04 -0600

    navbar logo fix

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation} from 'react-router-dom'
 import styles from './Nav.module.css'
-import logo from './../assets/ags-logo-black.svg';
+import logoDark from './../assets/ags-logo-dark.svg'
+import logoLight from './../assets/ags-logo-light.svg';
+
 
 function Nav() {
+
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <nav className={styles.nav}>
       <Link to="/" className={styles.link}>
         <div className={styles.left_links}>
           <div className={styles.logo_container}>
-            <img src={logo} alt="logo"></img>
+            <img src={isHomePage ? logoDark: logoLight} alt="logo"></img>
           </div>
         </div>
       </Link>
       
       <div className={styles.right_links}> 
-        <Link to="/solutions" className={styles.link}>solutions</Link>
-        <Link to="/services" className={styles.link}>services</Link>
+        {/* <Link to="/solutions" className={styles.link}>solutions</Link> */}
+        {/* <Link to="/services" className={styles.link}>services</Link> */}
         <Link to="/work" className={styles.link}>work</Link>
         <Link to="/contact" className={styles.link}>contact</Link>
       </div>

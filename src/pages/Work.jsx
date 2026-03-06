2b150ee848c472f1060b73000604a55125bff06b
Jacob Juarez <jacobjuarez1996@gmail.com>
2026-03-06 13:06:35 -0600

    two new gallery images

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -6,6 +6,8 @@ import privacy2 from "../assets/gallery-resized/privacy2.webp";
 import privacy3 from "../assets/gallery-resized/privacy3.webp";
 import privacy4 from "../assets/gallery-resized/privacy4.webp";
 import privacy5 from "../assets/gallery-resized/privacy5.webp";
+import privacy6 from "../assets/gallery-resized/privacy6.webp";
+
 import vinyl1 from "../assets/gallery-resized/vinyl1.webp"
 import vinyl2 from "../assets/gallery-resized/vinyl2.webp"
 import vinyl3 from "../assets/gallery-resized/vinyl3.webp"
@@ -14,19 +16,23 @@ import vinyl5 from "../assets/gallery-resized/vinyl5.webp"
 import vinyl6 from "../assets/gallery-resized/vinyl6.webp"
 import vinyl7 from "../assets/gallery-resized/vinyl7.webp"
 import vinyl8 from "../assets/gallery-resized/vinyl8.webp"
+
 import frame1 from "../assets/gallery-resized/frame1.webp"
 import frame2 from "../assets/gallery-resized/frame2.webp"
 import frame3 from "../assets/gallery-resized/frame3.webp"
 import frame4 from "../assets/gallery-resized/frame4.webp"
+
 import exterior1 from "../assets/gallery-resized/exterior1.webp"
 import exterior2 from "../assets/gallery-resized/exterior2.webp"
 import exterior3 from "../assets/gallery-resized/exterior3.webp"
+
 import lettering1 from "../assets/gallery-resized/lettering1.webp"
 import lettering2 from "../assets/gallery-resized/lettering2.webp"
 import lettering3 from "../assets/gallery-resized/lettering3.webp"
 import lettering4 from "../assets/gallery-resized/lettering4.webp"
 import lettering5 from "../assets/gallery-resized/lettering5.webp"
 import lettering6 from "../assets/gallery-resized/lettering6.webp"
+import lettering7 from "../assets/gallery-resized/lettering8.webp"
 
 
 
@@ -45,7 +51,7 @@ export default function Work() {
 
         <GallerySection
           title="Privacy Windows"
-          images={[privacy1, privacy2, privacy3, privacy4, privacy5]}
+          images={[privacy1, privacy2, privacy3, privacy4, privacy5, privacy6]}
         />
 
         <GallerySection
@@ -65,7 +71,7 @@ export default function Work() {
 
         <GallerySection
           title="Dimensional Lettering"
-          images={[lettering1, lettering2, lettering3, lettering4, lettering5, lettering6]}
+          images={[lettering1, lettering2, lettering3, lettering4, lettering5, lettering6, lettering7]}
         />
 
       </div>
